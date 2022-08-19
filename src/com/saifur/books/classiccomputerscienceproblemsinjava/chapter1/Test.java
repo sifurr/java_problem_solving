15540dfc535c731df024d74eb654cf2778810521
Md. Saifur Rahman <76596087+sifurr@users.noreply.github.com>
2022-08-19 14:20:06 +0600

    First commit

---
+++ src/com/saifur/books/classiccomputerscienceproblemsinjava/chapter1/Test.java
@@ -0,0 +1,12 @@
+package com.saifur.books.classiccomputerscienceproblemsinjava.chapter1;
+
+public class Test
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ src/com/saifur/books/classiccomputerscienceproblemsinjava/chapter1/examples/Fib1.java
@@ -0,0 +1,22 @@
+package com.saifur.books.classiccomputerscienceproblemsinjava.chapter1.examples;
+
+public class Fib1
+{
+	/**
+	 * This static method uses recursion, it will cause an error 
+	 * because it doesn't have any base case.
+	 * @param n it requires an integer as the i-th position
+	 * of a fibonacci number.
+	 */
+	private static int fib1(int n)
+	{
+		return fib1(n-1) + fib1(n-2);
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println(fib1(5));
+
+	}
+
+}
